c322918178c0a155d655f199e6616d251f2a23e4
GabrielSBrito17 <gsbbrito@hotmail.com>
2023-01-19 13:27:24 -0300

    Feat: adicionado listagem de usuarios.

--- ediaristas/administracao/views/usuario_views.py
+++ ediaristas/administracao/views/usuario_views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 def cadastrar_usuarios(request):
     if request.method == 'POST':
         form_usuario = UserCreationForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
+            return redirect('listar_usuarios')
     else:
         form_usuario = UserCreationForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+def listar_usuarios(request):
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
